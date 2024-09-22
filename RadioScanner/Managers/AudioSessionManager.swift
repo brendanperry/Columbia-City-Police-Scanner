d9512678addadaf8e4bb8d06ffada1fcf94d8e0f
Brendan Perry <brendan@brendanperry.me>
2024-09-22 15:34:48 -0400

    organize files and folders

---
+++ RadioScanner/Managers/AudioSessionManager.swift
@@ -0,0 +1,22 @@
+//
+//  AudioSessionManager.swift
+//  RadioScanner
+//
+//  Created by Brendan Perry on 9/19/24.
+//
+import AVKit
+
+struct AudioSessionManager {
+    let session = AVAudioSession.sharedInstance()
+    
+    func setLiveStreamAudio() {
+        try? session.setCategory(.playback,
+                                 mode: .default,
+                                 policy: .longFormAudio)
+        try? session.setActive(true)
+    }
+    
+    func audioStopped() {
+        try? session.setActive(false)
+    }
+}
